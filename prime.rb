3c43384953bda8af36bdf13ef30363a2a19ea23e
Tominiyi Ajayi <tominiyiajayi@gmail.com>
2019-01-21 23:13:43 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,20 @@
-# Add  code here!
+def prime?(number)
+  max = Math.sqrt(number)
+  (1..max).any? do |i| 
+    if number == 1
+      false
+    elsif number <= -1
+      false
+    elsif number % 1 == 0
+      false
+    else
+      true
+    end
+  end
+end
+
+
+
+    
+
+
