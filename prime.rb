8cd8580e3278258ffe7eccba3e680c72366c5c31
Tominiyi Ajayi <tominiyiajayi@gmail.com>
2019-01-21 23:35:54 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,12 +1,10 @@
 def prime?(number)
   if number >= 2
-    (2..number - 1).all? do |x|
-      number % x != 0
+    (2...number).all? do |i|
+      number % i != 0
     end
   else
     return false
   end
 end
     
-
-
